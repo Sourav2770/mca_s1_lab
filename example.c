a39045f22ce714c1cda4c5b0f5e41a6eebbabba3
Aaron Gladston <aarongladston2003@gmail.com>
2025-08-14 15:20:06 +0530

    added array deletion and insertion

--- example.c
+++ example.c
@@ -1,20 +1,54 @@
 #include <stdio.h>
 
 int main(){
-    int a[5];
+    int n;
+    printf("Enter the size of array : ");
+    scanf("%d", &n);
+    int a[n];
     
     printf("enter the elemets of ");
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<n;i++){
         // printf("%d \t",arr[i]);
         scanf("%d",&a[i]);
     }
     printf("elements : ");
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<n;i++){
         !
         printf("%d \t",a[i]);
     }
+    int pos;
+    printf("\n enter the position to delete");
+    scanf("%d",&pos);
+    pos--;
+    for(int i = pos; i <n-1; i++ ){
+        a[i]=a[i+1];
+    }
+    n--;
+
+    for(int i=0;i<n;i++){
+        printf("%d \t",a[i]);
+    }
+    printf("\n enter the position to insert : ");
+    scanf("%d",&pos);
+    pos--;
+
+    int arr[n+2];
+
+    for(int i = 0; i <n-1; i++ ){
+        arr[i]=a[i];
+    }
+
+    for(int i = pos; i <n-1; i++ ){
+        arr[i+1]=a[i];
+    }
+    printf("\n");
+    for (int i = 0; i <=n; i++)
+    {
+       printf(" %d",arr[i]);
+    }
+    
     return 0;
 
 }
