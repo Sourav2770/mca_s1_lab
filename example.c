eac6846470e3a40685a490ab224eb02363acdbf2
Aaron Gladston <aarongladston2003@gmail.com>
2025-08-14 14:37:25 +0530

    added array insertion and traversal

---
+++ example.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int a[5];
+    
+    printf("enter the elemets of ");
+
+    for(int i=0;i<5;i++){
+        // printf("%d \t",arr[i]);
+        scanf("%d",&a[i]);
+    }
+    printf("elements : ");
+
+    for(int i=0;i<5;i++){
+        !
+        printf("%d \t",a[i]);
+    }
+    return 0;
+
+}
