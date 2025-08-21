616829648e060a289e1c2087222b0c8e1a7d98f3
Akhil <akhilkb543@gmail.com>
2025-08-21 14:26:36 +0530

    modified logic

--- stacks.c
+++ stacks.c
@@ -1,46 +1,59 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#define MAX 100  
 int main()
 {
-    int n;
-    printf("Enter the size of array : ");
-    scanf("%d", &n);
-    int a[n];
-    
-    printf("enter the elemets of ");
+    int a[MAX];   
+    int top = -1;  
+    int choice, element;
 
-    for(int i=0;i<n;i++){
-        // printf("%d \t",arr[i]);
-        scanf("%d",&a[i]);
+    while (1)
+    {
+        printf("\n_________MENU_________ \n");
+        printf("1. PUSH \n");
+        printf("2. POP \n");
+        printf("3. PEEK \n");
+        printf("5. EXIT \n");
+        printf("ENTER YOUR CHOICE: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1: 
+            if(top==MAX-1){
+                printf("OVERFLOW\n");
+            }
+            else{
+                printf("enter the element to be inserted");
+                scanf("%d",&a[++top]);
+            }
+            break;
+
+            case 2:
+            if(top == -1)
+            {
+                printf("STACK UNDERFLOW \n");
+            }
+            else
+            {
+                a[--top];
+                printf("ELEMENT POPPED \n");
+
+            }
+            break;
+
+
+            case 3:
+                printf("Elements are : \t");
+                for (int i = 0; i <= top; i++)
+                {
+                    printf("%d \t",a[i]);
+                }
+                break;
+                
+            default:
+                break;
+
+        }
     }
-    printf("elements : ");
 
-    for(int i=0;i<n;i++){
-
-        printf("%d \t",a[i]);
-    }
-
-    
-    printf("enter the element to insert at top of stack");
-    scanf("%d",&a[n++]);
-
-    printf("elements : ");
-
-    for(int i=0;i<n;i++){
-
-        printf("%d \t",a[i]);
-    }
-
-
-    
-
-     a[n--];
-    printf("elements : ");
-
-    for(int i=0;i<n;i++){
-
-        printf("%d \t",a[i]);
-    }
-
-    return 0;
 }
